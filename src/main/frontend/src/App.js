c35755200ade4c6acd6bb8eb47ed11e734d5c8ca
bjmin17 <bjmin17@naver.com>
2022-10-20 14:48:15 +0900

    state 변경

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -7,23 +7,37 @@ import './App.css';
 function App() {
 
     let post = '강남 우동 맛집';
-    let [title, b] = useState(['남자 코트 추천','여자 코트 추천','코트 추천']);
+    let [title, updTitle] = useState(['남자 코트 추천','여자 코트 추천','코트 추천']);
+    let [like, updLike] = useState(0);
 
     return (
         <div className="App">
             <div className="black-nav">
                 <h4>{post}</h4>
             </div>
+
+            <button onClick={() => {
+                let copy = [...title];
+                copy[0] = '여자코트 추천';
+                updTitle(copy);
+            }}>글 수정</button>
+
+            <button onClick={() => {
+                let copyArr = [...title];
+                copyArr.sort();
+                updTitle(copyArr);
+            }}>가나다순정렬</button>
+
             <div className="list">
-                <h4>{title[0]}</h4>
+                <h4>{title[0]} <span onClick={() => {updLike(like+1)}}>👍</span> {like} </h4>
                 <p>10월 19일 발행</p>
             </div>
             <div className="list">
-                <h4>{title[1]}</h4>
+                <h4>{title[1]} <span>👍</span> 0 </h4>
                 <p>10월 19일 발행</p>
             </div>
             <div className="list">
-                <h4>{title[2]}</h4>
+                <h4>{title[2]} <span>👍</span> 0 </h4>
                 <p>10월 19일 발행</p>
             </div>
         </div>
