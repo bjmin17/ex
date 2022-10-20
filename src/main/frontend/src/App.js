9e8f024d5bd7dd2202bb055cefbd466cee59fdba
bjmin17 <bjmin17@naver.com>
2022-10-20 14:20:40 +0900

    useState 사용

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,21 +2,32 @@
 
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import './App.css';
 
 function App() {
-  const [hello, setHello] = useState('')
 
-  useEffect(() => {
-    axios.get('/api/hello')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-  }, []);
+    let post = '강남 우동 맛집';
+    let [title, b] = useState(['남자 코트 추천','여자 코트 추천','코트 추천']);
 
-  return (
-      <div>
-        백엔드에서 가져온 데이터입니다 : {hello}
-      </div>
-  );
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <h4>{post}</h4>
+            </div>
+            <div className="list">
+                <h4>{title[0]}</h4>
+                <p>10월 19일 발행</p>
+            </div>
+            <div className="list">
+                <h4>{title[1]}</h4>
+                <p>10월 19일 발행</p>
+            </div>
+            <div className="list">
+                <h4>{title[2]}</h4>
+                <p>10월 19일 발행</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
